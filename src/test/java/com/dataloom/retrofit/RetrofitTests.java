42b894770f92af128fb4f6146f06f23e1b9abd30
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-08 01:15:20 -0800

    Add basic test

---
+++ src/test/java/com/dataloom/retrofit/RetrofitTests.java
@@ -0,0 +1,32 @@
+package com.dataloom.retrofit;
+
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+
+public class RetrofitTests {
+    public static final Map<String, String> M = ImmutableMap.of("a","b");
+    @Test
+    public void testCreation() {
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addInterceptor( chain -> {
+                    chain.proceed( chain.request() );
+                    return new Response.Builder().build();
+                } )
+                .build();
+        Retrofit adapter = new Retrofit.Builder().baseUrl( "http://localhost:8081/rhizome/api/" ).client( httpClient )
+                .addConverterFactory( new LoomByteConverterFactory() )
+                .addConverterFactory( new LoomJacksonConverterFactory() )
+                .addCallAdapterFactory( new LoomCallAdapterFactory() ).build();
+        Api api = adapter.create( Api.class );
+        api.post( M );
+
+    }
+}
