ecc9b04e42f96784d83014cf9fd3bb9eb2a79627
Matthew Tamayo-Rios <matthew@openlattice.com>
2018-03-11 01:59:03 -0800

    Allow aws test configurations.

---
+++ src/main/java/com/openlattice/rhizome/configuration/AmazonTestingLaunchConfiguration.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2017. OpenLattice, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You can contact the owner of the copyright at support@openlattice.com
+ *
+ */
+
+package com.openlattice.rhizome.configuration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.kryptnostic.rhizome.configuration.annotation.ReloadableConfiguration;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author Matthew Tamayo-Rios &lt;matthew@kryptnostic.com&gt;
+ */
+@ReloadableConfiguration(
+        uri = "awstest.yaml" )
+public class AmazonTestingLaunchConfiguration {
+    public static final  String BUCKET_FIELD   = "bucket";
+    public static final  String FOLDER_FIELD   = "folder";
+    private static final String DEFAULT_FOLDER = "";
+    private final String bucket;
+    private final String folder;
+
+    public AmazonTestingLaunchConfiguration(
+            @JsonProperty( BUCKET_FIELD ) String bucket,
+            @JsonProperty( FOLDER_FIELD ) Optional<String> folder ) {
+        Preconditions.checkArgument( StringUtils.isNotBlank( bucket ),
+                "S3 bucket for configuration must be specified." );
+        this.bucket = bucket;
+        String rawFolder = folder.or( DEFAULT_FOLDER );
+
+        while ( StringUtils.endsWith( rawFolder, "/" ) ) {
+            StringUtils.removeEnd( rawFolder, "/" );
+        }
+        // We shouldn't prefix
+        if ( StringUtils.isNotBlank( rawFolder ) ) {
+            this.folder = rawFolder + "/";
+        } else {
+            this.folder = rawFolder;
+        }
+
+    }
+
+    @JsonProperty( BUCKET_FIELD )
+    public String getBucket() {
+        return bucket;
+    }
+
+    @JsonProperty( FOLDER_FIELD )
+    public String getFolder() {
+        return folder;
+    }
+
+    @Override
+    public String toString() {
+        return "AmazonLaunchConfiguration [bucket=" + bucket + ", folder=" + folder + "]";
+    }
+
+}
