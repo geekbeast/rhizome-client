502df245b8c93e8a5f44f8cfcdcc31032eec2a85
Matthew Tamayo-Rios <matthew@openlattice.com>
2019-02-27 11:54:12 -0800

    Better exception handling

--- src/main/java/com/openlattice/retrofit/RhizomeCallAdapterFactory.java
+++ src/main/java/com/openlattice/retrofit/RhizomeCallAdapterFactory.java
@@ -4,7 +4,6 @@ import com.google.common.base.Charsets;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.rmi.ServerException;
 import javax.annotation.Nullable;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
@@ -34,16 +33,20 @@ public class RhizomeCallAdapterFactory extends CallAdapter.Factory {
                         logger.error( "Call failed with code {} and message {} and error body {}",
                                 response.code(),
                                 response.message(),
-                                 responseBody);
-                        if( code >=500 && code<600) {
-                            throw new ServerException( responseBody );
+                                responseBody );
+                        if ( code >= 400 && code < 600 ) {
+                            final var message = call.request().url().toString();
+                            throw new RhizomeRetrofitCallException( "Retrofit API call to " + message + "failed",
+                                    responseBody,
+                                    response.code() );
                         }
                         return null;
                     }
                     return response.body();
                 } catch ( IOException e ) {
-                    logger.error( "Call failed.", e );
-                    return null;
+                    final var message = call.request().url().toString();
+                    throw new RhizomeRetrofitCallFailedException( "Retrofit call " + message + " failed.", e );
+
                 }
             }
 
